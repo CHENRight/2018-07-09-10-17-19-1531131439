b683afaf51746e3e3effd16194f458bef9aa9d23
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-11 20:34:12 +0800

    finish Practice07Test

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,39 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String teachCondition = klass == null ? "No Class" : klass.getDisplayName();
+        return super.introduce() + " " + String.format("I am a Teacher. I teach %s.", teachCondition);
+    }
+
+    public String introduceWith(Student student){
+        String result;
+        Boolean flag = klass != null && student.getKlass() != null && klass.getNumber() == student.getKlass().getNumber();
+        if(flag){
+            result = String.format(" I teach %s.", student.getName());
+        }else {
+            result = String.format(" I don't teach %s.", student.getName());
+        }
+        return super.introduce() + " I am a Teacher." + result;
+    }
 }
