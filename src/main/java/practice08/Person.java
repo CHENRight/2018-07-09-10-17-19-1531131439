6799e9b174e82e11e8a9b452a80b2f1724e39464
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-11 20:55:30 +0800

    finish Practice08Test

--- src/main/java/practice08/Person.java
+++ src/main/java/practice08/Person.java
@@ -1,4 +1,43 @@
 package practice08;
 
 public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(){
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Person){
+            Person temp = (Person)(obj);
+            return temp.id == this.id && temp.age == this.age && temp.name.equals(this.name);
+        }
+        return false;
+    }
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,26 @@
 package practice08;
 
-public class Student {
+
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce() {
+        boolean isLeader = klass != null && klass.getLeader() != null && klass.getLeader().getName().equals(this.getName());
+        if(isLeader){
+            return  super.introduce() + " " + String.format("I am a Student. I am Leader of %s.", klass.getDisplayName());
+        }else {
+            return super.introduce() + " " + String.format("I am a Student. I am at Class %d.", klass.getNumber());
+        }
+    }
 }
+
