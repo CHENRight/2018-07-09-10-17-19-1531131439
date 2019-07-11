a9f98e8b0b8e4c1bcbfd8744eef8035f130d6297
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-11 21:07:28 +0800

    finish Practice09Test

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,44 @@
 package practice09;
 
+
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", this.getNumber());
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public void assignLeader(Student student) {
+        Boolean flag = student.getKlass() == null || student.getKlass().getNumber() != this.number;
+        if (flag){
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = student;
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+    }
 }
