26283382874d0fdd64007f4994646310e6ad94be
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-11 21:22:44 +0800

    finish Practice10Test

--- src/main/java/practice10/Person.java
+++ src/main/java/practice10/Person.java
@@ -1,4 +1,48 @@
 package practice10;
 
+
 public class Person {
+    private String name;
+    private int age;
+    private int id;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Person(int id, String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce(){
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Person){
+            Person temp = (Person)(obj);
+            return temp.id == this.id && temp.age == this.age&& temp.name.equals(this.name);
+        }
+        return false;
+    }
 }
--- src/main/java/practice10/Student.java
+++ src/main/java/practice10/Student.java
@@ -1,4 +1,35 @@
 package practice10;
 
-public class Student {
+
+
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Student(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        boolean isLeader = klass != null && klass.getLeader() != null && klass.getLeader().getName().equals(this.getName());
+        if(isLeader){
+            return  super.introduce() + " " + String.format("I am a Student. I am Leader of %s.", klass.getDisplayName());
+        }else {
+            return super.introduce() + " " + String.format("I am a Student. I am at Class %d.", klass.getNumber());
+        }
+    }
 }
