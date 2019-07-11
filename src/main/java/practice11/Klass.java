ae0af7f7da47456db18a1cca5d84f2e1027187a0
REET CHEN (ITA-ISDC-ISD-OOCLL/ZHA) <CHENRE6@OOCL.COM>
2019-07-11 21:36:03 +0800

    finish Practice11Test

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,73 @@
 package practice11;
 
+
+import java.util.ArrayList;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private ArrayList<Student> students = new ArrayList<>();
+    private ArrayList<Teacher> teachers = new ArrayList<>();
+
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", this.getNumber());
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public ArrayList<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void setTeachers(ArrayList<Teacher> teachers) {
+        this.teachers = teachers;
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(ArrayList<Student> students) {
+        this.students = students;
+    }
+
+    public void assignLeader(Student student) {
+        Boolean flag = student.getKlass() == null || student.getKlass().getNumber() != this.number;
+        if (flag){
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = student;
+            this.teachers.forEach(curTeacher ->
+                    System.out.print(String.format("I am %s. I know %s become Leader of Class %d.\n", curTeacher.getName(), student.getName(), number))
+            );
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        students.add(student);
+        this.teachers.forEach(curTeacher ->
+            System.out.print(String.format("I am %s. I know %s has joined Class %s.\n", curTeacher.getName(), student.getName(), number))
+        );
+    }
+
+    public boolean isInClass(Student student){
+        Boolean flag = student.getKlass().number == this.number ? true : false;
+        return flag;
+    }
 }
--- src/main/java/practice11/Teacher.java
+++ src/main/java/practice11/Teacher.java
@@ -1,4 +1,72 @@
 package practice11;
 
-public class Teacher {
+import java.util.LinkedList;
+
+public class Teacher extends Person {
+    private LinkedList<Klass> classes;
+
+    public Teacher(String name, int age, LinkedList<Klass> classes) {
+        super(name, age);
+        this.classes = classes;
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> classes) {
+        super(id, name, age);
+        this.classes = classes;
+        for(Klass klass: classes){
+            klass.getTeachers().add(this);
+        }
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+
+    public LinkedList<Klass> getClasses() {
+        return classes;
+    }
+
+    @Override
+    public String introduce() {
+        String teachCondition;
+        if(classes != null && classes.size() > 0){
+            teachCondition = "Class";
+            for(int i = 0;i < classes.size();i++){
+                teachCondition += (" " + classes.get(i).getNumber() + "");
+                if(i != classes.size() - 1){
+                    teachCondition += ",";
+                }
+            }
+        }else{
+            teachCondition = "No Class";
+        }
+        return super.introduce() + " " + String.format("I am a Teacher. I teach %s.", teachCondition);
+
+    }
+
+    public String introduceWith(Student student){
+        String result;
+
+        Boolean flag = classes != null && classes.size() > 0 && isTeaching(student);
+        if(flag){
+            result = String.format(" I teach %s.", student.getName());
+        }else {
+            result = String.format(" I don't teach %s.", student.getName());
+        }
+        return super.introduce() + " I am a Teacher." + result;
+    }
+
+    public boolean isTeaching(Student student){
+        for(Klass klass : classes){
+            if(klass.isInClass(student)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
